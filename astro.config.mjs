3ffbc1a95ca9aa9c0ee9ad516cb63ca61dd7cdcc
anishshobithps <anish.shobith19@gmail.com>
2024-01-13 12:06:14 +0530

    vercel smh x5
    Signed-off-by: anishshobithps <anish.shobith19@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -48,4 +52,11 @@ export default defineConfig({
       brotli: true,
     }),
   ],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
