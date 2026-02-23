677530fad9fb03cac325bd52ab3fe426e9311803
anishshobithps <anish.shobith19@gmail.com>
2026-02-24 02:04:54 +0530

    fix: update Content-Security-Policy to allow script-src from cloud.umami.is

--- next.config.mjs
+++ next.config.mjs
@@ -47,7 +47,7 @@ const config = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloud.umami.is",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' data: https://fonts.gstatic.com",
               "img-src 'self' data: blob: https:",
