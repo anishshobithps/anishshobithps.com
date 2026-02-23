14e9c977344984b74abb147b5fd801c874113ea7
anishshobithps <anish.shobith19@gmail.com>
2026-02-23 18:42:29 +0530

    refactor: optimize BlogReadsInfo function to use Promise.all for concurrent data fetching

--- src/app/blog/[[...slug]]/blog-reads-info.tsx
+++ src/app/blog/[[...slug]]/blog-reads-info.tsx
@@ -3,8 +3,7 @@ import { TypographyMuted } from "@/components/ui/typography";
 import { Eye } from "lucide-react";
 
 export async function BlogReadsInfo({ slug }: { slug: string }) {
-  await trackRead(slug);
-  const reads = await getBlogReadsCount(slug);
+  const [reads] = await Promise.all([getBlogReadsCount(slug), trackRead(slug)]);
   return (
     <TypographyMuted
       className="font-mono text-xs flex items-center gap-1.5"
