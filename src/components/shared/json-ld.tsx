a913bca24d851ed696c4ce7f5e862db340a2d2b4
anishshobithps <anish.shobith19@gmail.com>
2026-04-06 13:24:54 +0530

    feat: add potentialAction for search functionality in buildWebSite

--- src/components/shared/json-ld.tsx
+++ src/components/shared/json-ld.tsx
@@ -72,6 +72,14 @@ function buildWebSite(): WithContext<WebSite> {
     url: siteConfig.baseUrl,
     description: siteConfig.description,
     author: schemaAuthor,
+    potentialAction: {
+      "@type": "SearchAction",
+      target: {
+        "@type": "EntryPoint",
+        urlTemplate: `${siteConfig.baseUrl}/blogs?q={search_term_string}`,
+      },
+      "query-input": "required name=search_term_string",
+    } as never,
   };
 }
 
