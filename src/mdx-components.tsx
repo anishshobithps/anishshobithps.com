28c53299266bbd0db71f3afe00458bf9f941a1c9
anishshobithps <anish.shobith19@gmail.com>
2026-07-17 13:35:41 +0530

    refactor(mdx): register TypographyMark and TypographyMuted globally

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,14 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import type { MDXComponents } from "mdx/types";
+import { TypographyMark, TypographyMuted } from "@/components/ui/typography";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    TypographyMark,
+    TypographyMuted,
     ...components,
   };
 }
